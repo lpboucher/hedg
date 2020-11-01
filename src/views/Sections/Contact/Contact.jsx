a77779359edbe600260c3306ad713c6cb99a1cfa
LP Boucher (AMS) <louis.boucher@achtzig20.de>
2020-11-01 19:55:38 +0100

    adjust contact

--- src/views/Sections/Contact/Contact.jsx
+++ src/views/Sections/Contact/Contact.jsx
@@ -21,7 +21,8 @@ const Contact = ({ className, frontmatter }) => {
   }
 
   const handleChange = e => {
-    setState({ [e.target.name]: e.target.value });
+    e.persist();
+    setState(existingState => ({ ...existingState, [e.target.name]: e.target.value }));
   };
 
   const handleSubmit = e => {
@@ -49,7 +50,6 @@ const Contact = ({ className, frontmatter }) => {
       <Row className="align-items-center justify-content-center">
         <Col>
           <div>
-            <h1>Contact</h1>
             <form
               name="contact"
               method="post"
@@ -58,35 +58,25 @@ const Contact = ({ className, frontmatter }) => {
               data-netlify-honeypot="bot-field"
               onSubmit={handleSubmit}
             >
-              {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
               <input type="hidden" name="form-name" value="contact" />
-              <p hidden>
+              <div className="sr-only">
                 <label>
-                  Don’t fill this out:{" "}
                   <input name="bot-field" onChange={handleChange} />
                 </label>
-              </p>
-              <p>
-                <label>
-                  Your name:<br />
-                  <input type="text" name="name" onChange={handleChange} />
-                </label>
-              </p>
-              <p>
-                <label>
-                  Your email:<br />
-                  <input type="email" name="email" onChange={handleChange} />
-                </label>
-              </p>
-              <p>
-                <label>
-                  Message:<br />
-                  <textarea name="message" onChange={handleChange} />
-                </label>
-              </p>
-              <p>
-                <button type="submit">Send</button>
-              </p>
+              </div>
+              <div className="form-group border-bottom my-5">
+                <label className="name-input" htmlFor="name-input">First Name & Last Name:</label>
+                <input onChange={handleChange} type="text" name="name" className="form-control bg-transparent border-0" id="name-input" />
+              </div>
+              <div className="form-group border-bottom my-5">
+                <label className="phone-input" htmlFor="phone-input">Phone number:</label>
+                <input onChange={handleChange} type="text" name="phone" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" className="form-control bg-transparent border-0" id="phone-input" />
+              </div>
+              <div className="form-group border-bottom my-5">
+                <label className="email-input" htmlFor="email-input">Email address:</label>
+                <input onChange={handleChange} type="email" name="email" className="form-control bg-transparent border-0" id="email-input" />
+              </div>
+              <Button size="xl" variant="secondary" className="btn-rounded">Submit</Button>
             </form>
           </div>
         </Col>
