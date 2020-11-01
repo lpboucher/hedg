7bd7a6526a4278bd42eecbfab8954d83db38410a
LP Boucher (AMS) <louis.boucher@achtzig20.de>
2020-11-01 16:46:46 +0100

    adjust form

--- src/views/Sections/Contact/Contact.jsx
+++ src/views/Sections/Contact/Contact.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
-import React from "react";
+import React, { useState } from "react";
+import { navigate } from "gatsby";
 import PropTypes from "prop-types";
 
 import { Row, Col, Form, Button } from "react-bootstrap";
@@ -7,10 +8,36 @@ import PageSection from "components/PageSection";
 import SectionHeader from "components/SectionHeader";
 import Image from "components/Image";
 
+function encode(data) {
+  return Object.keys(data)
+    .map((key) => `${encodeURIComponent(key)}=${encodeURIComponent(data[key])}`)
+    .join('&')
+}
+
 const Contact = ({ className, frontmatter }) => {
+  const [state, setState] = useState({});
   if (!frontmatter) {
     return null;
   }
+
+  const handleChange = e => {
+    setState({ [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    const form = e.target;
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({
+        "form-name": form.getAttribute("name"),
+        ...state
+      })
+    })
+      .then(() => navigate(form.getAttribute("action")))
+      .catch(error => alert(error));
+  };
 
   const { anchorId, header, subheader, imageFileName } = frontmatter;
 
@@ -21,42 +48,47 @@ const Contact = ({ className, frontmatter }) => {
       </Row>
       <Row className="align-items-center justify-content-center">
         <Col>
-          {/* <Form className="w-75" netlify-honeypot="bot-field" data-netlify="true" name="contact">
-            <input type="hidden" name="bot-field" />
-            <input type="hidden" name="form-name" value="contact" />
-            <Form.Group controlId="exampleForm.ControlInput1">
-              <Form.Label>First name & Last Name</Form.Label>
-              <Form.Control className="bg-transparent border-primary border-top-0 border-left-0 border-right-0 rounded-0" />
-            </Form.Group>
-            <Form.Group controlId="exampleForm.ControlInput1">
-              <Form.Label>Phone number</Form.Label>
-              <Form.Control className="bg-transparent border-primary border-top-0 border-left-0 border-right-0 rounded-0" />
-            </Form.Group>
-            <Form.Group controlId="exampleForm.ControlInput1">
-              <Form.Label>E-mail</Form.Label>
-              <Form.Control type="email" className="bg-transparent border-primary border-top-0 border-left-0 border-right-0 rounded-0" />
-            </Form.Group>
-  </Form> */}
-          <form name="contact" method="POST" data-netlify="true">
-            <div className="form-group">
-              <label htmlFor="name">Name:</label>
-              <input type="text" className="form-control" id="name" name="name" required />
-            </div>
-            <div className="form-group">
-              <label htmlFor="email">Email:</label>
-              <input type="email" className="form-control" id="email" name="email" required />
-            </div>
-            <div className="form-group">
-              <label htmlFor="subject">Subject:</label>
-              <input type="text" className="form-control" id="subject" name="subject" required />
-            </div>
-            <div className="form-group">
-              <label htmlFor="message">Message:</label>
-              <textarea className="form-control" rows="10" id="message" name="message" required />
-            </div>
-            <button type="submit" className="btn btn-primary">Submit</button>
-          </form>
-          <Button size="xl" variant="secondary" className="btn-rounded" type="submit">Submit</Button>
+          <div>
+            <h1>Contact</h1>
+            <form
+              name="contact"
+              method="post"
+              action="/thanks/"
+              data-netlify="true"
+              data-netlify-honeypot="bot-field"
+              onSubmit={handleSubmit}
+            >
+              {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
+              <input type="hidden" name="form-name" value="contact" />
+              <p hidden>
+                <label>
+                  Don’t fill this out:{" "}
+                  <input name="bot-field" onChange={handleChange} />
+                </label>
+              </p>
+              <p>
+                <label>
+                  Your name:<br />
+                  <input type="text" name="name" onChange={handleChange} />
+                </label>
+              </p>
+              <p>
+                <label>
+                  Your email:<br />
+                  <input type="email" name="email" onChange={handleChange} />
+                </label>
+              </p>
+              <p>
+                <label>
+                  Message:<br />
+                  <textarea name="message" onChange={handleChange} />
+                </label>
+              </p>
+              <p>
+                <button type="submit">Send</button>
+              </p>
+            </form>
+          </div>
         </Col>
         <Col className="text-center">
           <Image className="image" fileName={imageFileName} />
