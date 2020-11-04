20ea50a9b941a58521a3620b77af79247c10dd8b
LP Boucher (AMS) <louis.boucher@achtzig20.de>
2020-11-04 23:42:20 +0100

    add olivo script

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,7 +1,11 @@
-/**
- * Implement Gatsby's SSR (Server Side Rendering) APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/ssr-apis/
- */
+const React = require("react")
 
-// You can delete this file if you're not using it
+const additionalBodyElements = [
+  <div key="olivo" id="widget" />
+]
+
+exports.onRenderBody = ({
+  setPostBodyComponents
+}) => {
+  setPostBodyComponents(additionalBodyElements)
+}
