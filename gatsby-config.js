cecd81dec70e7f50bcc27600a1e5849e55b48cdb
LP Boucher (AMS) <louis.boucher@achtzig20.de>
2020-11-29 13:54:03 +0100

    update deprec plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -55,24 +55,13 @@ module.exports = {
       },
     },
     {
-      resolve: "gatsby-plugin-prefetch-google-fonts",
+      resolve: "gatsby-plugin-google-fonts",
       options: {
         fonts: [
-          {
-            family: "Montserrat",
-            variants: [400, 700],
-          },
-          {
-            family: "Kaushan+Script",
-          },
-          {
-            family: "Droid+Serif",
-            variants: [400, 700, "400italic", "700italic"],
-          },
-          {
-            family: "Roboto+Slab",
-            variants: [400, 100, 300, 700],
-          },
+          "Montserrat\:400,700",
+          "Kaushan+Script",
+          "Droid+Serif\:400,700,400i,700i",
+          "Roboto+Slab\:100,300,400,700",
         ],
       },
     },
